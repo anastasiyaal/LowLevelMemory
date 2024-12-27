f82f5f202dbaf1aee31cad01f10514946909bfcd
Anastasiia <mekhova.anastasiya@mail.ru>
2024-12-27 15:42:35 +0300

    Changed gitignore for mac.os Added default project files

---
+++ LowLevelMemory/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LowLevelMemory
+//
+//  Created by Анастасия Мехова on 27.12.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
